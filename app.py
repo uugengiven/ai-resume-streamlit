da4b5c88ec9ede95fdac41df9838d88c8a7eb840
John Lange <lange.john.m@gmail.com>
2023-08-24 20:23:54 -0400

    using form to limit execution

--- app.py
+++ app.py
@@ -43,17 +43,6 @@ chat_engine = index.as_chat_engine(
     verbose=True
     )
 
-
-with st.form("my_form"):
-   st.write("Inside the form")
-   my_number = st.slider('Pick a number', 1, 10)
-   my_color = st.selectbox('Pick a color', ['red','orange','green','blue','violet'])
-   st.form_submit_button('Submit my picks')
-
-# This is outside the form
-st.write(my_number)
-st.write(my_color)
-
 if prompt := st.chat_input("Your question"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
@@ -68,3 +57,12 @@ if st.session_state.messages[-1]["role"] != "assistant":
             st.write(response.response)
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message)
+
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   question = st.text_area('ask about john')
+   st.form_submit_button('Submit my picks')
+
+# This is outside the form
+st.write(question)
